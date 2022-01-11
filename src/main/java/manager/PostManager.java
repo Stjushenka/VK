9d4e87ab19ee71c49af1bb0178306aa5cd0831f9
Stjushenka <stjushenka@gmail.com>
2022-01-11 14:41:42 +0300

    PostManager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,14 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] postVK;
+
+    public Post[] searchWall(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void deleteWall(int ownerId, int postId) {
+    }
+}
